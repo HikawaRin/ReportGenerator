e4245a166701ff2299a8e14e663e1890bebb0deb
Rua <releaseavalon@163.com>
2019-01-24 15:14:13 +0800

    pull out DocumentModel from DocumentViewModel

---
+++ code/code/Model/DocumentModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace code.Model
+{
+    public class DocumentModel
+    {
+        public Word.Application _application { get; private set; }
+
+        public DocumentModel()
+        {
+            _application = Globals.ThisAddIn.Application;
+        }
+
+        public void AddBookMark(Word.Range range, string name)
+        {
+            range.Bookmarks.Add(name);
+        }
+    }
+}
--- code/code/ViewModel/DocumentViewModel.cs
+++ code/code/ViewModel/DocumentViewModel.cs
@@ -3,20 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using code.Model;
 using Word = Microsoft.Office.Interop.Word;
 
 namespace code.ViewModel
 {
     public class DocumentViewModel
     {
+        public DocumentModel DocumentModel { get; set; }
+
         public DocumentViewModel()
         {
+            DocumentModel = new DocumentModel();
         }
 
         public void InsertBookMark(string name)
         {
             Word.Cell c = Globals.ThisAddIn.Application.Selection.Cells[1];
-            c.Range.Bookmarks.Add(name);
+            DocumentModel.AddBookMark(c.Range, name);
         }
     }
 }
