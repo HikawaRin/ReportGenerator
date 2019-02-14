2dc4a5cfe4c4bf0aafef23c3b618c823b7e6261a
Rua <releaseavalon@163.com>
2019-02-14 21:13:06 +0800

    add recorder for persistencing MethodCall

---
+++ code/Record/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Record
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Recorder recorder = new Recorder(@"../../tape.xml", 1);
+
+            recorder.AddMethodCall(new MethodCall {
+                MethodName = "测试函数", 
+                Params = { new Param {
+                    ParamName = "测试参数",
+                    ValueType = "int", 
+                    Value = "44"
+                } }
+            });
+
+            recorder.Save();
+        }
+    }
+}
